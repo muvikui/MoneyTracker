487e2d314e33e66c0198a44ff9b40d3abbcaa3e7
Illia Rybachuk <muvikuimail@gmail.com>
2015-12-05 14:38:44 +0200

    - ND text color was added to colors - Container finder now stands in the beggining of method

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -22,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        container = (CoordinatorLayout) findViewById(R.id.container);
         setupDrawerLayout();
         setupToolbar();
-        container = (CoordinatorLayout) findViewById(R.id.container);
+
         Log.d(TAG, "Activity was Created.");
     }
 
