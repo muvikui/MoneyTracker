af326e6d0e0dd3e6b6b808ec7bd16d6be4989e35
Illia Rybachuk <muvikuimail@gmail.com>
2015-12-05 12:49:24 +0200

    Toolbar, navigation drawer, floating button were added.

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -1,21 +1,53 @@
 package com.loftschool.moneytracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LOG_TAG";
+    private Toolbar toolbar;
+    private CoordinatorLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setSupportActionBar(toolbar);
+        setupDrawerLayout();
+        setupActionBar();
+        
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         Log.d(TAG, "Activity was Created.");
     }
 
+    private void setupActionBar() {
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
     public void onStart() {
         super.onStart();
         Log.d(TAG, "Activity was Started.");
@@ -45,5 +77,30 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "Activity was Destroyed.");
     }
+
+    @Override
+    //Back system button
+    public void onBackPressed() {
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+   private void setupDrawerLayout() {
+       final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+       NavigationView view = (NavigationView) findViewById(R.id.nav_view);
+       view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+           @Override
+       public boolean onNavigationItemSelected(MenuItem menuItem) {
+               Snackbar.make(container, "Выбран пункт " + menuItem.getTitle(), Snackbar.LENGTH_SHORT).show();
+               menuItem.setChecked(true);
+               drawerLayout.closeDrawers();
+               return false;
+           }
+       });
+   }
 
 }
