adf54c104d43231d4015e26fe7fc97adfbdc3e3a
Illia Rybachuk <muvikuimail@gmail.com>
2015-12-08 16:34:58 +0200

    Changed Expenses list color.

--- app/src/main/java/com/loftschool/moneytracker/ExpensesAdapter.java
+++ app/src/main/java/com/loftschool/moneytracker/ExpensesAdapter.java
@@ -1,20 +1,21 @@
 package com.loftschool.moneytracker;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by Muvikui on 05.12.2015.
- */
 public class ExpensesAdapter extends ArrayAdapter<Expense> {
 
+    public static int list_color;
     List<Expense> expenses;
+
     public ExpensesAdapter(Context context, List<Expense> expenses) {
         super(context, 0, expenses);
         this.expenses = expenses;
@@ -24,15 +25,20 @@ public class ExpensesAdapter extends ArrayAdapter<Expense> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+
+
         Expense expense = getItem(position);
 
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
+        RelativeLayout color_item = (RelativeLayout) convertView.findViewById(R.id.list_item);
         TextView name = (TextView) convertView.findViewById(R.id.name_text);
         TextView sum = (TextView) convertView.findViewById(R.id.sum_text);
         TextView date = (TextView) convertView.findViewById(R.id.date_text);
+        color_item.setBackgroundColor(Color.parseColor("#FFFFBB33"));
+
 
         name.setText(expense.title);
         sum.setText(expense.getSum());
@@ -42,4 +48,7 @@ public class ExpensesAdapter extends ArrayAdapter<Expense> {
 
 
     }
+
+
+
 }
