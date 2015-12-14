c47b150b5c58ef1661aacb61af8595658a718884
Illia Rybachuk <muvikuimail@gmail.com>
2015-12-14 16:47:48 +0200

    Splash screen is created and working.

--- app/src/main/java/com/loftschool/moneytracker/StatisticsFragment.java
+++ app/src/main/java/com/loftschool/moneytracker/StatisticsFragment.java
@@ -1,29 +1,18 @@
 package com.loftschool.moneytracker;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.statistics_fragment)
 public class StatisticsFragment extends Fragment {
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
-        @ViewById
-        View view;
-        view = inflater.inflate(R.layout.statistics_fragment, container, false);
 
-    }
+        @AfterViews
+        void setTitle() {
+        getActivity().setTitle(R.string.nd_statistics);
 
-
-
-
-
-}
+}}
 
 //    private ExpensesAdapter expensesAdapter;
 //    View view;
